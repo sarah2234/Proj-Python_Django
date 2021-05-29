9bb872fc1aafd5e33eb1ba5e4fcabd5b432cc008
Sarah <2234sarah@gmail.com>
2021-05-30 06:13:56 +0900

    서버 테스트용 chromedriver 경로 변경+options 추가

--- website/time_table/views.py
+++ website/time_table/views.py
@@ -22,12 +22,17 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 
 # chrome driver를 불러오고 위의 option을 적용시킴
 # driver = webdriver.Chrome()  # 본인 컴퓨터에서 chromedrive가 있는 경로 입력
 # 입력예시
 driver = webdriver.Chrome(
-    '/Users/chisanahn/Desktop/Python_Project/chromedriver.exe',
+    '/home/ubuntu/venvs/mysite/bin/chromedriver',
     chrome_options=options)
 
 
