08691c0f467e1a317bda153c1944f36a38ee83be
Chisan Ahn <chisan2001@naver.com>
2021-06-01 18:41:15 +0900

    delete useless files

--- time_table/urls.py
+++ time_table/urls.py
@@ -6,23 +6,27 @@ from . import views
 app_name = 'time_table'
 
 urlpatterns = [
-    path('load/', views.load, name='load'),
-    # 학번, 비번 입력받고 블랙보드 공지사항 읽어오는 곳. 간단하게 확인가능
-    path('crawling/', views.crawling, name='crawling'),
     path('', views.schedule, name='schedule'),
-    path('<int:data_id>/delete', views.delete_assignment, name='delete_assignment'),
-    path('assignment_schedule/<int:assignment_id>', views.assignment_schedule, name='assignment_schedule'),
-    path('assignment_schedule/<int:assignment_id>/available_time', views.available_time, name='available_time'),
+    path('crawling/', views.crawling, name='crawling'),
+
     path('add_schedule', views.add_schedule, name='add_schedule'),
     path('add_schedule/add', views.add_function, name='add_function'),
+
     path('edit_schedule/<int:data_id>', views.edit_schedule, name='edit_schedule'),
     path('edit_schedule/<int:data_id>/delete', views.delete_function, name='delete_function'),
     path('edit_schedule/<int:data_id>/edit', views.edit_function, name='edit_function'),
-    path('setting', views.setting, name='setting'),
-    path('setting/timetable/load', views.load_timetable, name='load_timetable'),
-    path('setting/timetable/choose', views.choose_timetable, name='choose_timetable'),
+
+    path('<int:data_id>/delete', views.delete_assignment, name='delete_assignment'),
+    path('assignment_schedule/<int:assignment_id>', views.assignment_schedule, name='assignment_schedule'),
+    path('assignment_schedule/<int:assignment_id>/available_time', views.available_time, name='available_time'),
+
     path('cieat_interest', views.cieat_interest, name='cieat_interest'),
     path('cieat_interest/load', views.load_interest, name='load_interest'),
     path('cieat_interest/submit', views.cieat_submit, name='cieat_submit'),
+
+    path('setting', views.setting, name='setting'),
+    path('setting/timetable/load', views.load_timetable, name='load_timetable'),
+    path('setting/timetable/choose', views.choose_timetable, name='choose_timetable'),
+
     path('admin/', admin.site.urls),
 ]
--- time_table/views.py
+++ time_table/views.py
@@ -35,7 +35,7 @@ options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 # chrome driver를 불러오고 위의 option을 적용시킴
 # CHROMEDRIVER_PATH 환경 변수에 개발하면서 사용할 본인 컴퓨터 안의 chromedrive 경로 저장
-# os.environ["CHROMEDRIVER_PATH"] = '/Users/chisanahn/Desktop/Python_Project/chromedriver.exe'
+os.environ["CHROMEDRIVER_PATH"] = '/Users/chisanahn/Desktop/Python_Project/chromedriver.exe'
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
 
 date_list = ['월', '화', '수', '목', '금', '토', '일']
@@ -485,15 +485,6 @@ def edit_function(request, data_id):
     time = datetime.strptime(time_input, '%Y-%m-%d-%H:%M').replace(tzinfo=kst)
     Data(sort='개인일정', name=name, context=content, content=date_list[time.weekday()],
          time=time, start_h=int(start_h.split(":")[0]), end_h=int(end_h.split(":")[0])).save()
-    return redirect('time_table:schedule')
-
-
-def load(request):
-    """
-    Data 목록 출력
-    """
-    data_list = Data.objects.filter(sort='시간표').order_by('context')
-    context = {'data_list': data_list}
     return redirect('time_table:schedule')
 
 
