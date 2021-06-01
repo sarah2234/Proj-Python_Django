868bc32eca643de51e3f5a1659b28464bfc5e10a
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2021-06-02 01:25:25 +0900

    Add files via upload

---
+++ apps/common/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import SignUpForm, ProfileForm
+from django.urls import reverse_lazy
+  
+from .models import *
+from django.contrib import messages
+
+
+class HomeView(TemplateView):
+    template_name = 'common/home.html'
+
+class DashboardView(TemplateView):
+    template_name = 'common/dashboard.html'
+    login_url = reverse_lazy('login')
+
+
+from django.contrib.auth import authenticate, login
+
+def update_profile(request):
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+
+        if form.is_valid() and profile_form.is_valid():
+            
+            user = form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            
+            profile.save()
+            # username = form.cleaned_data.get('username')
+            # password = form.cleaned_data.get('password1')
+            # user = authenticate(request, username=username, password=password)
+            return redirect('login')
+
+    else:
+        form = SignUpForm()
+        profile_form = ProfileForm()
+    
+    context = {'form': form, 'profile_form': profile_form}
+    return render(request, 'common/register.html', context)
+
+
+# def register(request):
+#     form = SignUpForm()
+
+#     if request.method=='POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             return redirect('login') 
+
+#     context = {'form': form}          
+#     return render(request, 'common/register.html', context)    
+
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'common/login.html', {'error' : 'username or password is incorrect.'})
+    else:
+        return render(request, 'common/login.html')
+
+
+    
